a2c02e2973ef4f7bff7e359977c833e88226ead0
jokerjk092 <121729888+jokerjk092@users.noreply.github.com>
2023-01-05 22:22:05 +0530

    Delete Youtube_clone_server directory

--- Youtube_clone_server/models/commentModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-const Commented_videos=new mongoose.Schema({
-
-    videoid:String,
-    comments:{type:Array},
- 
-  
-});
-module.exports= mongoose.model("Commented_videos",Commented_videos);
--- Youtube_clone_server/models/fruit.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose')
-const fruitsSchema=new mongoose.Schema({
-  name:String,
-  rating:Number
-});
-module.exports= mongoose.model("Fruit",fruitsSchema);
-
